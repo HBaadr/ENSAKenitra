74d2e98fc79f9471a3efa554c1650c6c1bc9be4f
ayoubensalem <ayoubensalem@gmail.com>
2018-01-07 19:57:11 +0000

    gallery service

--- app/src/main/java/com/badr/hourimeche/ensakenitra/network/GetGalleriesDataService.java
+++ app/src/main/java/com/badr/hourimeche/ensakenitra/network/GetGalleriesDataService.java
@@ -1,8 +1,11 @@
 package com.badr.hourimeche.ensakenitra.network;
 
-/**
- * Created by neo on 07/01/18.
- */
+import com.badr.hourimeche.ensakenitra.models.Galleries;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
 
 public interface GetGalleriesDataService {
+    @GET("galleries")
+    Call<Galleries> getGalleries();
 }

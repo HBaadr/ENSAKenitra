6ddb1e69ea8331de2c42786209ed9c8b20f33060
ayoubensalem <ayoubensalem@gmail.com>
2018-01-07 18:43:07 +0000

    retrofit instance

--- app/src/main/java/com/badr/hourimeche/ensakenitra/network/RetrofitInstance.java
+++ app/src/main/java/com/badr/hourimeche/ensakenitra/network/RetrofitInstance.java
@@ -1,8 +1,20 @@
 package com.badr.hourimeche.ensakenitra.network;
 
-/**
- * Created by neo on 07/01/18.
- */
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://ensak-api.herokuapp.com/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
